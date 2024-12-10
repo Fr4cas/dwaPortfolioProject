2d9d6b26b303e6b8cd4e59e77727f31f4c3b0e58
Fracas <chenghsin2007@gmail.com>
2024-12-10 09:33:17 +0000

    slight changes to index and about page while also adding database creation file

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,12 @@
+# Create database script for Vocabulary app
+
+# Create the database
+CREATE DATABASE IF NOT EXISTS vocabulary_app;
+USE vocabulary_app;
+
+# Create the tables
+CREATE TABLE IF NOT EXISTS words (id INT AUTO_INCREMENT NOT NULL ,word VARCHAR(50),meaning VARCHAR(255),example_sentence VARCHAR(255),difficulty_level VARCHAR(255),category VARCHAR(100) PRIMARY KEY(id));
+
 # Create the user
 CREATE USER IF NOT EXISTS 'vocabulary_app_user'@'locahost' IDENTIFIED BY 'qwertyuiop';
 GRANT ALL PRIVILEGES ON vocabulary_app.* TO 'vocabulary_app_user'@'localhost';
